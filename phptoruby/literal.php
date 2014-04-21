b4fd5df633df10ea3d85de9f475ecd7885ae4eef
kanekomasanori <alley.cats.ml(a)gmail.com>
2014-04-21 19:54:17 +0900

    学習用コード追加

---
+++ phptoruby/literal.php
@@ -0,0 +1,18 @@
+<?php
+
+// 数値リテラル
+$number = 999;
+echo $number;
+
+// 文字列リテラル
+$str = "Hello";
+echo $str;
+
+// ダブルクォートで囲まれた文字列では変数が展開されます
+echo "Hello $number";
+
+// 配列リテラル
+echo array(1,2,3);
+
+// 連想配列リテラル
+echo array('name' => 'TARO', 'age' => 20);
