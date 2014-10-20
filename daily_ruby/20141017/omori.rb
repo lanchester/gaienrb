ea9a9094c965f0e6ec9be60a24a2a621a91a6730
omori.yasutaka <omori.yasutaka@lanches.co.jp>
2014-10-20 17:00:18 +0900

    no message

--- daily_ruby/20141017/omori.rb
+++ daily_ruby/20141017/omori.rb
@@ -1,8 +1,20 @@
 #1から100まで足した数の合計を出力するプログラム
 
-sum = 0
-for i in 1..100
-	sum = sum + i
-end
+# for を使用したパターン
 
-puts sum
+sum1 = 0
+for i in 1..100
+	sum1 = sum1 + i
+end
+puts sum1
+
+# while を使用したパターン
+
+sum2 = 0
+i = 1
+while i <= 100
+	sum2 = sum2 + i
+	i = i + 1
+end
+puts sum2
+
