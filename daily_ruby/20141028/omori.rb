70942818c08d07c5f30f142adb70765789a6c2ba
omori.yasutaka <omori.yasutaka@lanches.co.jp>
2014-10-31 13:58:19 +0900

    no message

--- daily_ruby/20141028/omori.rb
+++ daily_ruby/20141028/omori.rb
@@ -2,15 +2,24 @@
 ファイルを追記するプログラムです
 
 コマンドライン引数が渡されると、渡された引数をファイルに追記していきます。
+-----
+1,引数がない場合は、ファイルの内容を表示します。
+2,ファイルに何もない場合は、「ファイルは空です」と表示します。
 
-引数がない場合は、ファイルの内容を表示します。
-複数の引数が渡された場合は、複数の引数を追記します。
+3,複数の引数が渡された場合は、複数の引数を追記します。
 
-ファイルに何もない場合は、「ファイルは空です」と表示します。
-
-保存するファイル名は自由です。
+・保存するファイル名は自由です。
 =end
-
-File.open("omori_text.txt", "w") do |io|
-  p io.puts ARGV[0]
-end
+io = File.open("omori_text.txt","a+")
+	if ARGV.size != 0
+		while line = io.puts(ARGV)
+		end
+	elsif io.size == 0
+		puts "ファイルは空です"
+	else 
+		while line = io.gets
+		line.chomp!
+		puts line 
+		end
+	end
+io.close
